d5f1eccc1da0350dc570a86ce4be4ed7c20e386a
plopcan <127615799+plopcan@users.noreply.github.com>
2024-06-01 18:48:54 +0200

    Add files via upload

---
+++ NOFK/trabajo_it.sql
@@ -0,0 +1,176 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-06-2024 a las 18:47:43
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `trabajo_it`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ascensos`
+--
+
+CREATE TABLE `ascensos` (
+  `ID` int(11) NOT NULL,
+  `cargo` varchar(50) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date DEFAULT NULL,
+  `jefe_departamento` varchar(20) NOT NULL,
+  `requisitos` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `ID` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `gerente` varchar(20) DEFAULT NULL,
+  `descripcion` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `departamento`
+--
+
+INSERT INTO `departamento` (`ID`, `nombre`, `gerente`, `descripcion`) VALUES
+(1, 'oficina', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `DNI` varchar(20) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `cargo` varchar(50) DEFAULT NULL,
+  `departamento` int(11) NOT NULL,
+  `id_ascensos` int(11) DEFAULT NULL,
+  `contrasena` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`DNI`, `nombre`, `apellidos`, `direccion`, `cargo`, `departamento`, `id_ascensos`, `contrasena`) VALUES
+('12345678K', 'Juan', 'Perez', NULL, 'gerente', 1, NULL, 'juan');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluacion`
+--
+
+CREATE TABLE `evaluacion` (
+  `ID` int(11) NOT NULL,
+  `DNI` varchar(20) NOT NULL,
+  `anyo` int(11) NOT NULL,
+  `companyerismo` int(11) NOT NULL,
+  `eficiencia` int(11) NOT NULL,
+  `iniciativa` int(11) NOT NULL,
+  `liderazgo` int(11) NOT NULL,
+  `nota_media` decimal(3,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nomina`
+--
+
+CREATE TABLE `nomina` (
+  `ID` int(11) NOT NULL,
+  `salario` decimal(10,2) NOT NULL,
+  `DNI` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `nomina`
+--
+
+INSERT INTO `nomina` (`ID`, `salario`, `DNI`) VALUES
+(1, 100.00, '12345678K');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vacaciones`
+--
+
+CREATE TABLE `vacaciones` (
+  `ID` int(11) NOT NULL,
+  `DNI` varchar(20) NOT NULL,
+  `fecha_inicio` date NOT NULL,
+  `fecha_fin` date NOT NULL,
+  `motivo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `ascensos`
+--
+ALTER TABLE `ascensos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`DNI`);
+
+--
+-- Indices de la tabla `evaluacion`
+--
+ALTER TABLE `evaluacion`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `vacaciones`
+--
+ALTER TABLE `vacaciones`
+  ADD PRIMARY KEY (`ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
